86b1a5545753adb2ea76ce5e60fa95551b779e2d
jojo-cornbread <josiahrambally13@gmail.com>
2024-02-14 01:30:02 +0000

    created outline for models and controllers

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,7 @@ from App.controllers import ( create_user, get_all_users_json, get_all_users,
 
 # This commands file allow you to create convenient CLI commands for testing controllers
 
+
 app = create_app()
 migrate = get_migrate(app)
 
